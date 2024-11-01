a29bcc5219b8d18118c2d556041b3b8bf030f685
kimamovic21 <imamovic.kerim@gmail.com>
2024-11-01 17:31:08 +0100

    13. Navbar Component
    -kreiran folder components i komponenta Navbar.tsx
    -u komponentu layout.tsx dodana komponenta Navbar.tsx

--- tutorial/app/layout.tsx
+++ tutorial/app/layout.tsx
@@ -1,11 +1,14 @@
 import './globals.css';
+import Navbar from '@/components/Navbar';
 
 export default function RootLayout({ children } : { children: React.ReactNode }) {
   return (
     <html lang='en'>
       <body>
-        <nav>hello there</nav>
-        {children}
+        <Navbar />
+        <main className='max-w-3xl mx-auto py-10'>
+          {children}
+        </main>
       </body>
     </html>
   );
