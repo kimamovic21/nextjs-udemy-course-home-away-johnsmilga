debdbb8c966addfb9190f61e211a750ba80b9838
kimamovic21 <imamovic.kerim@gmail.com>
2024-11-02 11:47:51 +0100

    17. Counter
    -kreiran folder counter i komponenta page.tsx
    -u folderu components kreirana komponenta Counter.tsx
    -u komponentu counter/page.tsx dodana komponenta Counter.tsx

---
+++ tutorial/app/counter/page.tsx
@@ -0,0 +1,12 @@
+import Counter from '@/components/Counter';
+
+function CounterPage() {
+  return (
+    <section>
+      <h2 className='text-6xl mb-16'>Page Content</h2>
+      <Counter />
+    </section>
+  );
+};
+
+export default CounterPage;
