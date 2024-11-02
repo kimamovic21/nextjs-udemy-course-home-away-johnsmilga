15aa505fe04077e0ad2b587e77df23e438b76849
kimamovic21 <imamovic.kerim@gmail.com>
2024-11-02 15:33:42 +0100

    30. Actions File
    -kreiran folder utils i actions.ts fajl
    -u actions.ts fajlu kreirana i eksportovana funkcija createUser()
    -u komponentu Form.tsx dodana funkcija createUser

---
+++ tutorial/utils/actions.ts
@@ -0,0 +1,9 @@
+'use server';
+
+export const createUser = async (formData: FormData) => {
+  const firstName = formData.get('firstName') as string;
+  const lastName = formData.get('lastName') as string;
+  const rawData = Object.fromEntries(formData);
+  console.log(rawData);
+  console.log({ firstName, lastName });
+};
