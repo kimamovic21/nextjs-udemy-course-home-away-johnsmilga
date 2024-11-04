bc921ee95aa8e985d2fe7e8275d15bcb95e214d3
kimamovic21 <imamovic.kerim@gmail.com>
2024-11-04 17:14:10 +0100

    36. Delete Button
    -u folderu components kreirana komponenta DeleteButton.tsx
    -u komponentu UsersList.tsx dodana komponenta DeleteButton.tsx

--- tutorial/components/UsersList.tsx
+++ tutorial/components/UsersList.tsx
@@ -1,4 +1,5 @@
 import { fetchUsers } from '@/utils/actions';
+import DeleteButton from './DeleteButton';
 
 async function UsersList() {
   const users = await fetchUsers();
@@ -6,10 +7,14 @@ async function UsersList() {
   return (
     <div className='mt-4'>
       {users.length ? (
-        <div>
+        <div className='max-w-lg'>
           {users.map((user) => (
-            <h4 key={user.id} className='capitalize text-lg'>
+            <h4
+              key={user.id}
+              className='capitalize text-lg flex justify-between items-center mb-2'
+            >
               {user.firstName} {user.lastName}
+              <DeleteButton id={user.id} />
             </h4>
           ))}
         </div>
