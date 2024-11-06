6f27e80288806a305ab1d526cd3d36a6f51e9b46
kimamovic21 <imamovic.kerim@gmail.com>
2024-11-06 14:52:08 +0100

    59. LinksDropdown Component
    -kreiran folder utils i links.ts fajl
    -u komponentu UserIcon.tsx dodana React ikonica LuUser2
    -u komponentu LinksDropdown.tsx dodane komponente za dropdown menu

--- home-away/components/navbar/LinksDropdown.tsx
+++ home-away/components/navbar/LinksDropdown.tsx
@@ -1,6 +1,40 @@
+import { LuAlignLeft } from 'react-icons/lu';
+import { Button } from '../ui/button';
+import { links } from '@/utils/links';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import Link from 'next/link';
+import UserIcon from './UserIcon';
+
 function LinksDropdown() {
   return (
-    <div>LinksDropdown</div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant='outline' className='flex gap-4 max-w-[100px]'>
+          <LuAlignLeft className='w-6 h-6' />
+          <UserIcon />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='w-52' align='start' sideOffset={10}>
+        {links.map((link) => {
+          return (
+            <Link
+              key={link.href}
+              href={link.href}
+              className="capitalize w-full"
+            >
+              <DropdownMenuItem>
+                {link.label}
+              </DropdownMenuItem>
+            </Link>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
 
