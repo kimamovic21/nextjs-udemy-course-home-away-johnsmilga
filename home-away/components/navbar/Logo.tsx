ea830421ab0cdb60597c58dc63cdc923a1f10d6e
kimamovic21 <imamovic.kerim@gmail.com>
2024-11-06 11:04:31 +0100

    56. Logo and NavSearch Components
    -u terminalu instaliran paket React Icons
    -u komponentu Logo.tsx dodane komponente Link, Button i LuTent
    -u komponentu NavSearch.tsx dodana komponenta Input

--- home-away/components/navbar/Logo.tsx
+++ home-away/components/navbar/Logo.tsx
@@ -1,6 +1,14 @@
+import { LuTent } from 'react-icons/lu';
+import { Button } from '../ui/button';
+import Link from 'next/link';
+
 function Logo() {
   return (
-    <div>Logo</div>
+    <Button size='icon' asChild>
+      <Link href='/'>
+        <LuTent className='w-6 h-6' />
+      </Link>
+    </Button>
   );
 };
 
