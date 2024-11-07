765ac2e18cbb6b9ac0d545f4b38640956c0dafdc
kimamovic21 <imamovic.kerim@gmail.com>
2024-11-07 13:54:49 +0100

    68. Refactor Create Profile
    -u komponentu create/page.tsx dodane komponente SubmitButton iz komponente Buttons.tsx, FormContainer.tsx i FormInput.tsx

--- home-away/app/profile/create/page.tsx
+++ home-away/app/profile/create/page.tsx
@@ -1,7 +1,13 @@
-const createProfileAction = async (formData: FormData) => {
+import { SubmitButton } from '@/components/form/Buttons';
+import FormContainer from '@/components/form/FormContainer';
+import FormInput from '@/components/form/FormInput';
+
+const createProfileAction = async (prevState: any, formData: FormData) => {
   'use server';
   const firstName = formData.get('firstName') as string;
   console.log(firstName);
+  // if (firstName !== 'shakeAndBake') return { message: 'there was an error...' };
+  return { message: 'Profile Created' };
 };
 
 function CreateProfilePage() {
@@ -9,9 +15,14 @@ function CreateProfilePage() {
     <section>
       <h2 className='text-2xl font-semibold mb-8 capitalize'>new user</h2>
       <div className='border p-8 rounded-md max-w-lg'>
-        <form action={createProfileAction}>
-          {/* Form Container Component */}
-        </form>
+        <FormContainer action={createProfileAction}>
+          <div className='grid gap-4 mt-4 '>
+            <FormInput type='text' name='firstName' label='First Name' />
+            <FormInput type='text' name='lastName' label='Last Name' />
+            <FormInput type='text' name='username' label='Username' />
+          </div>
+          <SubmitButton text='Create Profile' className='mt-8' />
+        </FormContainer>
       </div>
     </section>
   );
