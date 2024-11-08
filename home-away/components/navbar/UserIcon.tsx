bb00e74df374d9ac72742664f7567d18df6c2416
kimamovic21 <imamovic.kerim@gmail.com>
2024-11-08 14:46:16 +0100

    76. Fetch Profile Image Action
    -u actions.ts fajlu kreirana i eksportovana asinhrona funkcija fetchProfileImage()
    -u komponentu UserIcon.tsx dodana funkcija fetchProfileImage i komponenta Image
    -u next.config.mjs fajlu dodan objekt images

--- home-away/components/navbar/UserIcon.tsx
+++ home-away/components/navbar/UserIcon.tsx
@@ -1,6 +1,21 @@
 import { LuUser2 } from 'react-icons/lu';
+import { fetchProfileImage } from '@/utils/actions';
+import Image from 'next/image';
 
-function UserIcon() {
+async function UserIcon() {
+  const profileImage = await fetchProfileImage();
+
+  if (profileImage)
+    return (
+      <Image 
+        src={profileImage} 
+        alt='User image'
+        width={100}
+        height={100}
+        className='w-6 h-6 rounded-full object-cover' 
+      />
+    );
+    
   return (
     <LuUser2 className='w-6 h-6 bg-primary rounded-full text-white' />
   );
