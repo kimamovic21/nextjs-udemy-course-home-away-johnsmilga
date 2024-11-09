015b3f78ed8619ff528d59388c3795b557f67dbf
kimamovic21 <imamovic.kerim@gmail.com>
2024-11-09 14:39:54 +0100

    83. Image Input Container
    - actions.ts fajlu kreirana i eksportovana asinhrona funkcija updateProfileImageAction()
    -kreirana komponenta ImageInputContainer.tsx
    -u komponenti Buttons.tsx kreiran tip btnSize
    -u komponentu profile/page.tsx dodana komponenta  ImageInputContainer.tsx

--- home-away/utils/actions.ts
+++ home-away/utils/actions.ts
@@ -108,3 +108,10 @@ export const updateProfileAction = async (
     return renderError(error);
   };
 };
+
+export const updateProfileImageAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  return { message: 'Profile image updated successfully' };
+};
