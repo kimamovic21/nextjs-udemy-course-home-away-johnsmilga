c837198960541769e6675074e02aad875c23a236
kimamovic21 <imamovic.kerim@gmail.com>
2024-11-13 13:22:09 +0100

    103. Properties Container
    -u komponentu PropertiesContainer.tsx dodana funkcija fetchProperties, tip PropertyCardProps, komponente PropertiesList.tsx i EmptyList.tsx

--- home-away/components/home/PropertiesContainer.tsx
+++ home-away/components/home/PropertiesContainer.tsx
@@ -1,7 +1,29 @@
-function PropertiesContainer() {
-  return (
-    <div>PropertiesContainer</div>
-  );
+import { fetchProperties } from '@/utils/actions';
+import { type PropertyCardProps } from '@/utils/types';
+import PropertiesList from './PropertiesList';
+import EmptyList from './EmptyList';
+
+async function PropertiesContainer({
+  category,
+  search,
+}: {
+  category?: string;
+  search?: string;
+}) {
+  const properties: PropertyCardProps[] = await fetchProperties({ category, search });
+  console.log(properties);
+  
+  if (properties.length === 0) {
+    return (
+      <EmptyList
+        heading='No results.'
+        message='Try changing or removing some of your filters.'
+        btnText='Clear Filters'
+      />
+    );
+  };
+
+  return <PropertiesList properties={properties} />;
 };
 
 export default PropertiesContainer;
