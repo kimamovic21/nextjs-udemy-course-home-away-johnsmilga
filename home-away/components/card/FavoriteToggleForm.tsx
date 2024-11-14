0cc8a13e40bd5febf7ff2cae4aae63970e98b296
kimamovic21 <imamovic.kerim@gmail.com>
2024-11-14 15:31:42 +0100

    113. Favorites Toggle Form
    -u komponenti Buttons.tsx kreirana i eksportovana funkcija CardSubmitButton()
    -u actions.ts fajlu funkciji toggleFavoriteAction() dodan parametar prevState
    -u komponentu FavoriteToggleForm.tsx dodane funkcije usePathname, toggleFavoriteAction, komponente FormContainer.tsx i CardSubmitButton iz komponente Buttons.tsx

--- home-away/components/card/FavoriteToggleForm.tsx
+++ home-away/components/card/FavoriteToggleForm.tsx
@@ -1,6 +1,31 @@
-function FavoriteToggleForm() {
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { toggleFavoriteAction } from '@/utils/actions';
+import { CardSubmitButton } from '../form/Buttons';
+import FormContainer from '../form/FormContainer';
+
+type FavoriteToggleFormProps = {
+  propertyId: string;
+  favoriteId: string | null;
+};
+
+function FavoriteToggleForm({
+  propertyId,
+  favoriteId,
+}: FavoriteToggleFormProps) {
+  const pathname = usePathname();
+
+  const toggleAction = toggleFavoriteAction.bind(null, {
+    propertyId,
+    favoriteId,
+    pathname,
+  });
+
   return (
-    <div>FavoriteToggleForm</div>
+    <FormContainer action={toggleAction}>
+      <CardSubmitButton isFavorite={favoriteId ? true : false} />
+    </FormContainer>
   );
 };
 
