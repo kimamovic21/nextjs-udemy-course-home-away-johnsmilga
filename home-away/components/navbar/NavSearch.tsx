5db022d63632649909c1395684be48a33e29a67f
kimamovic21 <imamovic.kerim@gmail.com>
2024-11-14 13:22:06 +0100

    110. NavSearch Component
    -u terminalu intaliran paket Use Debounce
    -u komponentu NavSearch.tsx dodana logika za pretragu nekretnina

--- home-away/components/navbar/NavSearch.tsx
+++ home-away/components/navbar/NavSearch.tsx
@@ -1,11 +1,45 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 import { Input } from '../ui/input';
 
 function NavSearch() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const [search, setSearch] = useState(
+    searchParams.get('search')?.toString() || ''
+  );
+
+  const handleSearch = useDebouncedCallback((value: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (value) {
+      params.set('search', value);
+    } else {
+      params.delete('search');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
+  useEffect(() => {
+    if (!searchParams.get('search')) {
+      setSearch('');
+    };
+  }, [searchParams.get('search')]);
+
   return (
     <Input
       type='search'
       placeholder='find a property...'
       className='max-w-xs dark:bg-muted '
+      onChange={(e) => {
+        setSearch(e.target.value);
+        handleSearch(e.target.value);
+      }}
+      value={search}
     />
   );
 };
