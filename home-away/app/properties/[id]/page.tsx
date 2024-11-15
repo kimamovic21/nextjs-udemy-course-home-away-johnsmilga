b67eea7d6b0af05543c19015f607946a231f2720
kimamovic21 <imamovic.kerim@gmail.com>
2024-11-15 14:22:22 +0100

    118. Share Button Component
    -u terminalu instaliran paket React Share
    -u .env fajlu kreirana env varijabla NEXT_PUBLIC_WEBSITE_URL
    -u .env.local.example fajlu dodan primjer varijable koju treba kreirati
    -u folderu properties kreirana komponenta ShareButton.tsx
    -u komponentu components/properties kreirana komponenta ShareButton.tsx

--- home-away/app/properties/[id]/page.tsx
+++ home-away/app/properties/[id]/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from 'next/navigation';
 import { fetchPropertyDetails } from '@/utils/actions';
 import BreadCrumbs from '@/components/properties/BreadCrumbs';
 import FavoriteToggleButton from '@/components/card/FavoriteToggleButton';
+import ShareButton from '@/components/properties/ShareButton';
 
 async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   const property = await fetchPropertyDetails(params.id);
@@ -16,7 +17,7 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
       <header className='flex justify-between items-center mt-4'>
         <h2 className='text-4xl font-bold '>{property.tagline}</h2>
         <div className='flex items-center gap-x-4'>
-          {/* share button */}
+          <ShareButton name={property.name} propertyId={property.id} />
           <FavoriteToggleButton propertyId={property.id} />
         </div>
       </header>
