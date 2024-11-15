8b44d611179187d5aa114690aeab79673d8fd317
kimamovic21 <imamovic.kerim@gmail.com>
2024-11-15 10:34:28 +0100

    116. Property Details Page - Setup
    -u actions.ts fajlu kreirana i eksportovana asinhrona funkcija fetchPropertyDetails()
    -u folderu properties kreiran folder [id] i komponenta page.tsx
    -u folderu properties kreirana komponenta loading.tsx
    -izbrisana komponenta properties/page.tsx

---
+++ home-away/app/properties/[id]/page.tsx
@@ -0,0 +1,16 @@
+import { redirect } from 'next/navigation';
+import { fetchPropertyDetails } from '@/utils/actions';
+
+async function PropertyDetailsPage({ params }: { params: { id: string } }) {
+  const property = await fetchPropertyDetails(params.id);
+  if (!property) redirect('/');
+  
+  const { baths, bedrooms, beds, guests } = property;
+  const details = { baths, bedrooms, beds, guests };
+  
+  return (
+    <div>PropertyDetailsPage</div>
+  );
+};
+
+export default PropertyDetailsPage;
--- home-away/app/properties/page.tsx
+++
@@ -1,7 +0,0 @@
-function PropertiesPage() {
-  return (
-    <h2 className='text-3xl'>Properties Page</h2>
-  );
-};
-
-export default PropertiesPage;
