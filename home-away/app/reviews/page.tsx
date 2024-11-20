dacde892fddf80d39f2d742f26734c35af85c5d9
kimamovic21 <imamovic.kerim@gmail.com>
2024-11-20 13:11:25 +0100

    137. Reviews Page
    -u komponentu reviews/page.tsx dodana logika za prikazivanje recenzija za korisnika po id-u

--- home-away/app/reviews/page.tsx
+++ home-away/app/reviews/page.tsx
@@ -1,6 +1,48 @@
-function ReviewsPage() {
+import { deleteReviewAction, fetchPropertyReviewsByUser } from '@/utils/actions';
+import { IconButton } from '@/components/form/Buttons';
+import EmptyList from '@/components/home/EmptyList';
+import Title from '@/components/properties/Title';
+import ReviewCard from '@/components/reviews/ReviewCard';
+import FormContainer from '@/components/form/FormContainer';
+
+async function ReviewsPage() {
+  const reviews = await fetchPropertyReviewsByUser();
+  if (reviews.length === 0) {
+    return <EmptyList />
+  };
+
   return (
-    <h2 className='text-3xl'>Reviews Page</h2>
+    <>
+      <Title text='Your Reviews' />
+      <section className='grid md:grid-cols-2 gap-8 mt-4 '>
+        {reviews.map((review) => {
+          const { comment, rating } = review;
+          const { name, image } = review.property;
+          const reviewInfo = {
+            comment,
+            rating,
+            name,
+            image,
+          };
+
+          return (
+            <ReviewCard key={review.id} reviewInfo={reviewInfo}>
+              <DeleteReview reviewId={review.id} />
+            </ReviewCard>
+          );
+        })}
+      </section>
+    </>
+  );
+};
+
+const DeleteReview = ({ reviewId }: { reviewId: string }) => {
+  const deleteReview = deleteReviewAction.bind(null, { reviewId });
+
+  return (
+    <FormContainer action={deleteReview}>
+      <IconButton actionType='delete' />
+    </FormContainer>
   );
 };
 
