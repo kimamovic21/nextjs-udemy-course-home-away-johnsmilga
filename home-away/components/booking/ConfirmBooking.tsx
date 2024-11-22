4cb07900ef2c692dad2f939efa3cc60c9105afa9
kimamovic21 <imamovic.kerim@gmail.com>
2024-11-22 16:40:35 +0100

    147. Confirm Booking Component
    -u actions.ts fajlu kreirana i eksportovana asinhrona funkcija createBookingAction()
    -u komponentu ConfirmBooking.tsx dodane funkcije useAuth, useProperty, createBookingAction, komponente SignInButton, Button, SubmitButton i FormContainer.tsx

--- home-away/components/booking/ConfirmBooking.tsx
+++ home-away/components/booking/ConfirmBooking.tsx
@@ -1,6 +1,41 @@
+'use client';
+
+import { useAuth, SignInButton } from '@clerk/nextjs';
+import { useProperty } from '@/utils/store';
+import { createBookingAction } from '@/utils/actions';
+import { Button } from '@/components/ui/button';
+import { SubmitButton } from '@/components/form/Buttons';
+import FormContainer from '@/components/form/FormContainer';
+
 function ConfirmBooking() {
+  const { userId } = useAuth();
+  const { propertyId, range } = useProperty((state) => state);
+
+  const checkIn = range?.from as Date;
+  const checkOut = range?.to as Date;
+
+  if (!userId) {
+    return (
+      <SignInButton mode='modal'>
+        <Button type='button' className='w-full'>
+          Sign In to Complete Booking
+        </Button>
+      </SignInButton>
+    );
+  };
+
+  const createBooking = createBookingAction.bind(null, {
+    propertyId,
+    checkIn,
+    checkOut,
+  });
+
   return (
-    <div>ConfirmBooking</div>
+    <section>
+      <FormContainer action={createBooking}>
+        <SubmitButton text='Reserve' className='w-full' />
+      </FormContainer>
+    </section>
   );
 };
 
