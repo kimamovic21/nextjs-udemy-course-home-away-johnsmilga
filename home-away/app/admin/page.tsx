47ab4bdd1dfe37b20b40d6c6c74e6fba1e4f5a2c
kimamovic21 <imamovic.kerim@gmail.com>
2024-12-03 16:41:48 +0100

    166. Admin Page - Loading Components
    -u komponentu admin/page.tsx dodane komponenta Suspense i cetiri tsx komponente iz foldera components
    -u komponenti admin/Loading.tsx kreirane tri funkcije: LoadingCard(), ChartsLoadingContainer() i StatsLoadingContainer()

--- home-away/app/admin/page.tsx
+++ home-away/app/admin/page.tsx
@@ -1,6 +1,18 @@
+import { Suspense } from 'react';
+import { ChartsLoadingContainer, StatsLoadingContainer } from '@/components/admin/Loading';
+import ChartsContainer from '@/components/admin/ChartsContainer';
+import StatsContainer from '@/components/admin/StatsContainer';
+
 function AdminPage() {
   return (
-    <div>AdminPage</div>
+    <>
+      <Suspense fallback={<StatsLoadingContainer />}>
+        <StatsContainer />
+      </Suspense>
+      <Suspense fallback={<ChartsLoadingContainer />}>
+        <ChartsContainer />
+      </Suspense>
+    </>
   );
 };
 
--- home-away/components/admin/Loading.tsx
+++ home-away/components/admin/Loading.tsx
@@ -1,7 +1,28 @@
-function Loading() {
+import { Card, CardHeader } from '@/components/ui/card';
+import { Skeleton } from '@/components/ui/skeleton';
+
+function LoadingCard() {
   return (
-    <div>Loading</div>
+    <Card>
+      <CardHeader>
+        <Skeleton className='w-full h-20 rounded' />
+      </CardHeader>
+    </Card>
+  );
+}
+
+export function ChartsLoadingContainer() {
+  return (
+    <Skeleton className='mt-16 w-full h-[300px] rounded' />
   );
 };
 
-export default Loading;
+export function StatsLoadingContainer() {
+  return (
+    <div className='mt-8 grid md:grid-cols-2 gap-4 lg:grid-cols-3'>
+      <LoadingCard />
+      <LoadingCard />
+      <LoadingCard />
+    </div>
+  );
+};
