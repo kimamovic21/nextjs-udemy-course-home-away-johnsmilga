d8ea6c0f34356d4ebb54fa78f7941107f253b24d
kimamovic21 <imamovic.kerim@gmail.com>
2024-12-04 16:50:43 +0100

    171. Stripe - Checkout Page
    -u komponentu page.tsx dodana funkcija loadStripe, komponente EmbeddedCheckoutProvider i EmbeddedCheckout iz paketa Stripe

--- home-away/app/checkout/page.tsx
+++ home-away/app/checkout/page.tsx
@@ -1,6 +1,36 @@
+'use client';
+
+import { useCallback } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { loadStripe } from '@stripe/stripe-js';
+import { EmbeddedCheckoutProvider, EmbeddedCheckout } from '@stripe/react-stripe-js';
+import axios from 'axios';
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
+);
+
 function CheckoutPage() {
+  const searchParams = useSearchParams();
+
+  const bookingId = searchParams.get('bookingId');
+
+  const fetchClientSecret = useCallback(async () => {
+    // Create a Checkout Session
+    const response = await axios.post('/api/payment', {
+      bookingId: bookingId,
+    });
+    return response.data.clientSecret;
+  }, []);
+
+  const options = { fetchClientSecret };
+  
   return (
-    <h2 className='text-3xl'>Checkout Page</h2>
+    <div id='checkout'>
+      <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+        <EmbeddedCheckout />
+      </EmbeddedCheckoutProvider>
+    </div>
   );
 };
 
